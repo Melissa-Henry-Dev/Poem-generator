936e2959420bfc280f6bbfecef4a4bbd2fb4e8ab
Melissa-Henry-Dev <melissa.henry0@gmail.com>
2025-04-17 12:48:55 +0200

    user experience

--- src/script.js
+++ src/script.js
@@ -1,13 +1,28 @@
-function generatePoem(event) {
+function displayPoem(response) {
+
+    new Typewriter("#poem", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+}
+  
+  function generatePoem(event) {
     event.preventDefault();
   
-    new Typewriter("#poem", {
-        strings: "Where the ocean meets the sky",
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-    });
-
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "14efb8tb73d93975b84df7e7fa474a0o";
+    let context =
+      "You are a Poem expert and love to write short poems. You mission is to generate a 4 line poem and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
+  
+    axios.get(apiURL).then(displayPoem);
 }
   
 let poemFormElement = document.querySelector("#poem-generator-form");
